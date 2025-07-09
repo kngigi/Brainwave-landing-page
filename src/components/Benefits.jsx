53bcffde9ea107e7cb16e6162c8e7c25ab1d5cac
kngigi <92370374+kngigi@users.noreply.github.com>
2025-07-09 14:52:17 +0300

    Update Benefits Layout

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -13,7 +13,7 @@ const Benefits = () => {
         <Heading className="md:max-w-md lg:max-w-2xl"
                  title="Chat Smarter, Not Harder with Brainwave"/>
 
-        <div className='flex flex-wrap gap-10 mb-10'>
+        <div className='flex flex-wrap gap-10 mb-10 justify-center'>
           {benefits.map((item)=>(
             <div key={item.id} 
                  className='block relative p-0.5 bg-no-repeat 
